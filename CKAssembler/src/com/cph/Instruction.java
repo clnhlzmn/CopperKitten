3915856fddd1df616f51295ad8de6b55470ac27e
Colin Holzman <colin.holzman@protonmail.com>
2019-01-17 15:04:41 -0500

    remove old CKAssembler project

--- CKAssembler/src/com/cph/Instruction.java
+++
@@ -1,9 +0,0 @@
-package com.cph;
-
-//represents a concrete instruction
-public interface Instruction extends PseudoInstruction {
-
-    //emit the instructions in a form determined in part by TargetContext
-    String emit(TargetContext targetContext);
-
-}
