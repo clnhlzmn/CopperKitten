9d24971a01c7ddd3054ffc9c94d8673a37c0d7ea
Colin Holzman <colin.holzman@protonmail.com>
2019-02-16 18:24:57 -0500

    need to change the way program array is initialized

--- assembler/src/OutputContext.kt
+++ assembler/src/OutputContext.kt
@@ -9,6 +9,7 @@ class OutputContext {
 
     fun emit(): String {
         return "${layoutFunctions.map { lf -> lf.emit() }.fold("") { acc, s -> "$acc\n$s"}}\n" +
+            //TODO: program array can't be initialized at compile time with arithmetic on addresses
             "uint8_t program[] = {\n" +
             program.map { op -> "\t$op,\n" }.fold("") { acc, s -> acc + s } +
             "};"
