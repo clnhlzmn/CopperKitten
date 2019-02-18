c7f6845746c30f2941edad879d2489d7dbee3401
Colin Holzman <colin.holzman@protonmail.com>
2019-02-17 19:52:47 -0500

    add apache cli to cka

---
+++ assembler/src/Cli.kt
@@ -0,0 +1,72 @@
+
+import org.antlr.v4.runtime.CharStreams
+import org.antlr.v4.runtime.CommonTokenStream
+import org.apache.commons.cli.*;
+import java.util.Collections.addAll
+import java.util.ArrayList
+import java.io.File
+import org.antlr.v4.runtime.BailErrorStrategy
+
+
+
+
+class Cli(val args: Array<String>) {
+
+    private val options = Options()
+
+    fun Cli() {
+        options.addOption("h", "help", false, "show help")
+        //        options.addOption("c", "check", true, "check file");
+        //        options.addOption("i", "interpret", true, "interpret file");
+    }
+
+    fun parse() {
+
+        val parser = DefaultParser()
+
+        var cmd: CommandLine? = null
+        try {
+            cmd = parser.parse(options, args)
+
+            if (cmd!!.hasOption("h")) {
+                help()
+            }
+
+            //create based on options passed
+            val tc = TargetContext({ mnemonic -> "(enum vm_op_code)${mnemonic.toUpperCase()}" }, 4)
+
+            //TODO: input and output file from options
+            if (cmd.getArgs().size == 1) {
+                val stream = CharStreams.fromFileName(cmd.args[0])
+                val lexer = ckaLexer(stream)
+                val tokens = CommonTokenStream(lexer)
+                val ckaParser = ckaParser(tokens)
+                ckaParser.errorHandler = BailErrorStrategy()
+                val context = ckaParser.file()
+                val pc = FileVisitor().visit(context)
+                //TODO: memSize and stackSize from options
+                val oc = OutputContext(1000, 100)
+                for (inst in pc.instructions) {
+                    inst.emit(pc, tc, oc)
+                }
+                println(oc.emit())
+            } else {
+                help()
+            }
+
+        } catch (e: ParseException) {
+            help()
+        } catch (e: RuntimeException) {
+            e.printStackTrace()
+        }
+
+    }
+
+    private fun help() {
+        // This prints out some help
+        val formater = HelpFormatter()
+        formater.printHelp("h", options)
+        System.exit(0)
+    }
+}
+
--- assembler/src/Main.kt
+++ assembler/src/Main.kt
@@ -1,33 +1,5 @@
-import org.antlr.v4.runtime.*
 
-val stream = CharStreams.fromString(
-    "push -1    //start at -1\n" +
-        "Loop:      //begin loop here\n" +
-        "push 1\n" +
-        "add       //add 1\n" +
-        "dup\n" +
-        "push 26\n" +
-        "cmp       //cmp to 27\n" +
-        "jumpz End //if equal then end\n" +
-        "dup\n" +
-        "push 65\n" +
-        "add       //else add 'A'\n" +
-        "out       //print\n" +
-        "jump Loop\n" +
-        "End:\n" +
-        "halt"
-)
-val lexer = ckaLexer(stream)
-val tokens = CommonTokenStream(lexer)
-val parser = ckaParser(tokens)
-val context = parser.file()
-
-fun main() {
-    val tc = TargetContext({ mnemonic -> "(enum vm_op_code)${mnemonic.toUpperCase()}" }, 8)
-    val pc = FileVisitor().visit(context)
-    val oc = OutputContext(1000, 100)
-    for (inst in pc.instructions) {
-        inst.emit(pc, tc, oc)
-    }
-    println(oc.emit())
+fun main(args: Array<String>) {
+    val temp = Array(1) { "C:\\code\\ck\\ck\\example.cka" }
+    Cli(temp).parse();
 }
