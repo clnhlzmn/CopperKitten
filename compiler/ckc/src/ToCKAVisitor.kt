59c7e370c5455012da56ea6ff157a189a9519632
Colin Holzman <colinholzman@Colins-MacBook-Pro.local>
2019-03-03 19:22:43 -0500

    to CKA

--- compiler/ckc/src/ToCKAVisitor.kt
+++ compiler/ckc/src/ToCKAVisitor.kt
@@ -1,12 +1,25 @@
 
 
+fun compileTopLevel(expr: Expr) {
+
+}
+
 class ToCKAVisitor : ASTVisitor<Unit> {
     //to translate top level expression to cka:
     //treat the top level expr as body in the following expr
     //{():Unit body}()
+
+    //
     //enter         //create top level frame
     //layout [0]    //first local is ref
-    //alloc [2, 0]  //alloc array for function
-    //
+
+    //push 1        //push size of function array
+    //alloc []      //alloc array for function (no captures)
+    //push Begin    //push address of entry point
+
+    //leave
+
+
+
 }
 
